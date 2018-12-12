1040291d42e6082db748de31d57512614f7feb28
sgermosen <jsmentales@gmail.com>
2018-12-12 16:55:25 -0400

    creation of the models and 1st controller

--- WebApiNetCore/Startup.cs
+++ WebApiNetCore/Startup.cs
@@ -5,10 +5,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using WebApiNetCore.Models;
 
 namespace WebApiNetCore
 {
@@ -24,6 +26,9 @@ namespace WebApiNetCore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var connection = Configuration.GetConnectionString("ChinookDb"); //this is the name than we give to the label than is going to have the value of the connection string
+            services.AddDbContext<ChinookContext>(options => options.UseSqlServer(connection));//then we are going to inject our context
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
